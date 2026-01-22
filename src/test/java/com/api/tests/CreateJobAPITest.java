7109697cfb1b99b935deee245948c01eeed99b2d
Saurabh-LetsCode <saurabh.jasani@gmail.com>
2026-01-22 15:43:36 +0530

    Introduced ENUMS for handling codes for the create job API test
    Reduced magic number issue in the test script

--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -2,7 +2,7 @@ package com.api.tests;
 
 import  static io.restassured.RestAssured.*;
 
-import com.api.constant.Role;
+import com.api.constant.*;
 
 import com.api.request.model.*;
 import static com.api.utils.DateTimeUtil.*;
@@ -25,12 +25,12 @@ public class CreateJobAPITest {
 
         Customer customer = new Customer("Jatin","Jasani","7507331188","","saurabh.jasani@gmail.com","");
         CustomerAddress customerAddress = new CustomerAddress("801","Vasant  Galaxy","Balewadi Street","Near orchid school","Balewadi","411006","Indian","MH");
-        CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10),"12308899001005","12308899001005","12308899001005",getTimeWithDaysAgo(10),1,1);
-        Problems problems = new Problems(1,"Battery Issue");
+        CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10),"12308877001005","12308877001005","12308877001005",getTimeWithDaysAgo(10), Product.NEXUS_2.getCode(), Model.NEXUS_2_BLUE.getCode());
+        Problems problems = new Problems(Problem.SMARTPHONE_IS_RUNNING_SLOW.getCode(),"Battery Issue");
         List<Problems> problemsList = new ArrayList<>();
         problemsList.add(problems);
 
-        CreateJobPayload createJobPayload = new CreateJobPayload(0,2,1,1,customer,customerAddress,customerProduct,problemsList);
+        CreateJobPayload createJobPayload = new CreateJobPayload(ServiceLocation.SERVICE_LOCATION_A.getCode(),Platform.FRONT_DESK.getCode(),Warranty_Status.IN_WARRANTY.getCode(), OEM.GOOGLE.getCode(),customer,customerAddress,customerProduct,problemsList);
 
         given().spec(SpecUtil.requestSpecWithAuth(Role.FD,createJobPayload))
                 .when()
