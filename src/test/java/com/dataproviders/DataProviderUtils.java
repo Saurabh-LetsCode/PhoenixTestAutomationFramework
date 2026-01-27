1d91d579c89f8ca7ad2b935bfc9cd8ca081e79b7
Saurabh-LetsCode <saurabh.jasani@gmail.com>
2026-01-27 06:58:02 +0530

    Completed Requirement 8 - Date driven testing with csv
    Added below tests
    1 - LoginAPIDataDriven
    2-  CreateJobAPIDataDrivenTest

--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -1,10 +1,15 @@
 package com.dataproviders;
 
+import com.api.request.model.CreateJobPayload;
 import com.api.utils.CSVReaderUtil;
+import com.api.utils.CreateJobBeanMapper;
+import com.dataproviders.api.bean.CreateJobBean;
 import com.dataproviders.api.bean.UserBean;
 import org.testng.annotations.DataProvider;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class DataProviderUtils {
 
@@ -12,7 +17,22 @@ public class DataProviderUtils {
     @DataProvider(name = "LoginAPIDataProvider",parallel = true)
     public static Iterator<UserBean> loginAPIDataProvider(){
 
-        return CSVReaderUtil.loadCSV("testData/LoginCreds.csv");
+        return CSVReaderUtil.loadCSV("testData/LoginCreds.csv", UserBean.class);
+
+    }
+
+
+    @DataProvider(name = "CreateJobAPIDataProvider",parallel = true)
+    public static Iterator<CreateJobPayload> createJobDataProvider(){
+
+        Iterator<CreateJobBean> createJobBeanIterator =  CSVReaderUtil.loadCSV("testData/CreateJobData.csv", CreateJobBean.class);
+        List<CreateJobPayload>  createJobPayloadList = new ArrayList<>();
+
+        while (createJobBeanIterator.hasNext()){
+            createJobPayloadList.add(CreateJobBeanMapper.mapper(createJobBeanIterator.next()));
+        }
+
+        return createJobPayloadList.iterator();
 
     }
 }
